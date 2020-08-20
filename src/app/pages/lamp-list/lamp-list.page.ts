fdd36418a13846ae10751412bc1c33722a49194a
ucok pasaribu <ucok.pasaribu@axiasolusi.com>
2020-08-20 17:47:18 +0700

    fix lampservice

--- src/app/pages/lamp-list/lamp-list.page.ts
+++ src/app/pages/lamp-list/lamp-list.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LamplistService } from 'src/app/services/lamplist.service';
 import { LampList } from 'src/models/LampList.model';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lamp-list',
@@ -10,17 +12,56 @@ import { LampList } from 'src/models/LampList.model';
 export class LampListPage implements OnInit {
   lampList: LampList[];
 
-  constructor(private lampListService: LamplistService) { }
+  constructor(
+    private lampListService: LamplistService,
+    private alertCtrl: AlertController,
+    private router: Router,
+    private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
-    this.lampListService.lamps.subscribe(data => {
+  }
+
+  ionViewWillEnter() {
+    this.lampListService.getLampList.subscribe(data => {
       this.lampList = data;
       console.log(this.lampList);
+    });
+  }
+
+  changeStatus(value, deviceCode) {
+    let status = value.detail.checked === true ? 'on' : 'off';
+    
+    this.alertCtrl.create({
+      header: `Are you sure to turn ${status} this lamp?`,
+      buttons: [
+        {
+          text: 'Okay',
+          handler: () => {
+            this.updateLampStatus(deviceCode, value.detail.checked);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.updateLampStatus(deviceCode, !value.detail.checked);
+          }
+        }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
     })
   }
 
-  changeStatus(value) {
-    console.log(value.detail.checked);
+  updateLampStatus(deviceCode, value) {
+    this.loadingCtrl.create({
+      message: 'Updating Lamp Status ...'
+    }).then(loadingEl => {
+      loadingEl.present();
+      this.lampListService.updateStatusLamp(deviceCode, value).subscribe(updateLampList => {
+        loadingEl.dismiss();
+      });
+    });
   }
 
 }
--- src/app/services/lamplist.service.ts
+++ src/app/services/lamplist.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LampList } from 'src/models/LampList.model';
 import { BehaviorSubject } from 'rxjs';
+import { tap, take, switchMap, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,28 +17,28 @@ export class LamplistService {
     ),
     new LampList(
       'Lampu Belajar',
-      'WTCBAT2020062500001',
+      'WTCBAT2020062500002',
       'Kosan Tebet, Jl, Tebet Barat No. 106 J',
       'Floor = 2, Room = 4, Spot = Atas Meja Komputer',
       2
     ),
     new LampList(
       'Lampu A',
-      'WTCBAT2020062500001',
+      'WTCBAT2020062500003',
       'Kosan Tebet, Jl, Tebet Barat No. 106 J',
       'Floor = 2, Room = 4, Spot = Atas Meja A',
       2
     ),
     new LampList(
       'Lampu B',
-      'WTCBAT2020062500001',
+      'WTCBAT2020062500004',
       'Kosan Tebet, Jl, Tebet Barat No. 106 J',
       'Floor = 2, Room = 4, Spot = Atas Meja B',
       1
     ),
     new LampList(
       'Lampu C',
-      'WTCBAT2020062500001',
+      'WTCBAT2020062500005',
       'Kosan Tebet, Jl, Tebet Barat No. 106 J',
       'Floor = 2, Room = 4, Spot = Atas Meja C',
       2
@@ -45,4 +46,35 @@ export class LamplistService {
   ]);
 
   constructor() { }
+
+  get getLampList() {
+    return this.lamps.asObservable();
+  }
+
+  updateStatusLamp(deviceCode, status) {
+    let updateStatus = status === true ? 1 : 2;
+
+    return this.getLampList.pipe(
+      take(1),
+      map(lampList => {
+        let updatePlaceIndex = lampList.findIndex(list => list.device_code === deviceCode);
+        let updatedLampList = [...lampList];
+
+        const oldLamp = lampList[updatePlaceIndex];
+
+        updatedLampList[updatePlaceIndex] = new LampList(
+          oldLamp.bat_name,
+          oldLamp.device_code,
+          oldLamp.site,
+          oldLamp.site_detail,
+          updateStatus
+        );
+
+        return updatedLampList;
+      }),
+      tap(updateLampList => {
+        this.lamps.next(updateLampList);
+      })
+    );
+  }
 }
